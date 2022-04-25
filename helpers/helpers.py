6d50a706a51d9581c89867b910876f03bf4568cc
Sherlock Wisdom <wisdomnji@gmail.com>
2022-04-25 19:35:18 +0100

    Dividing by 16 rather 8 for 32 bytes hex issues

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -3,8 +3,9 @@
 
 import secrets
 
-def generate_shared_key(keysize: int=256//8) -> str:
+def generate_shared_key(keysize: int=256//16) -> str:
     """Generates a shared key.
+    Why //16? https://stackoverflow.com/a/50321063
 
     Args:
         keysize (int): size of key (in bits) to generate (defaults to 256).

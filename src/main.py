6e616bdfe5610191127c6e3edcb7f7a58377f611
sherlock <wisdomnji@gmail.com>
2023-03-28 15:35:37 +0000

    update: delete endpoint tested

--- src/main.py
+++ src/main.py
@@ -105,6 +105,17 @@ CORS(
 #
 #    return response
 
+@app.after_request
+def after_request(response):
+    response.headers["Strict-Transport-Security"] = "max-age=63072000; includeSubdomains"
+    response.headers["X-Content-Type-Options"] = "nosniff"
+    response.headers["Content-Security-Policy"] = "script-src 'self'; object-src 'self'"
+    response.headers["Referrer-Policy"] = "strict-origin-when-cross-origin"
+    response.headers["Cache-Control"] = "no-cache"
+    response.headers["Permissions-Policy"] = "accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), clipboard-read=(), clipboard-write=(), cross-origin-isolated=(), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(), gamepad=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), speaker=(), speaker-selection=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=()"
+    return response
+
+
 @app.route('/v%s/sync/users/<user_id>' % (__api_version_number), methods=['GET'])
 def get_sync_url(user_id: str):
     """
@@ -175,6 +186,10 @@ def verify_user_shared_key(msisdn_hash: str):
             app.logger.exception(error)
             return '', 403 
         else:
+
+            if not user.shared_key:
+                return 'no shared key for user', 403
+
             user_shared_key = user.shared_key
             user_public_key = user.public_key
 
